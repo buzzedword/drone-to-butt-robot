82251e76d4349ff70e921c748be634db7efb5ab8
Daniel Garcia <danny@noisenewyork.com>
2013-12-02 14:38:48 -0500

    Updated for plural.

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,19 +1,19 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -27,13 +27,15 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
 	v = v.replace(/\bDrone\b/g, "Butt Robot");
+	v = v.replace(/\bDrones\b/g, "Butt Robots");
 	v = v.replace(/\bdrone\b/g, "butt robot");
-	
+	v = v.replace(/\bdrones\b/g, "butt robots");
+
 	textNode.nodeValue = v;
 }
 
